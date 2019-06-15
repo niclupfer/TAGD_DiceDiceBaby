6b34c31eb0c86cdd9969143eb9f8abd438acb690
James <jpa234@yahoo.com>
2019-06-14 19:19:49 -0500

    quick and dirty player class
    Went ahead and threw together a simple player class. Not sure how we are implementing it yet. I stored mana as a vector, imo it'll help us keep all the mana in one place and organized.

---
+++ DiceDiceBaby/Assets/Scenes/James/Scripts/James_Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class James_Player
+{
+    public string name;
+
+    public int score;
+    public Vector3 mana; // red, green, blue
+    public James_DiceBody[] diceDraft;
+}
