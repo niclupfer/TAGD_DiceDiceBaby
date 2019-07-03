78ee684df273e3aef365d84595f565c4abd2c542
James <jpa234@yahoo.com>
2019-07-03 15:36:25 -0500

    dicedraft clone + circle dice script
    Check the James folder for DiceDraftDraft2, that's where I put my new script. The script is sitting on the empty gameobject holding all the dice.

---
+++ DiceDiceBaby/Assets/Scenes/James/Scripts/James_CircularDice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class James_CircularDice : MonoBehaviour
+{
+    //placed on central DICE object, controls rotation and placement of dice in a ring.
+    public Chris_GameController gameController;
+
+    private int dieTot;
+
+    private void Start()
+    {
+        dieTot = gameController.dicePool.Count;
+    }
+
+    void PutDiceInRing()
+    {
+        float r = Mathf.Sin(0.5f * (360f/(float)dieTot));
+        for (int d = 0; d < dieTot; d++)
+        {
+            Transform dicePlace = gameController.dicePool[d].transform;
+            float angle = d * (360f / (float)dieTot);
+            dicePlace.localPosition = new Vector3(r * Mathf.Cos(angle),0,r*Mathf.Sin(angle));
+        }
+    }
+}
