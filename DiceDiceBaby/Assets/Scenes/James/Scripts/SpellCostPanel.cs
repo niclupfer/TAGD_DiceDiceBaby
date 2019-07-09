40dc869089c63c4db874fa12e27378f9f571a2b0
James <jpa234@yahoo.com>
2019-07-09 16:01:35 -0500

    panel controller for spell list

--- DiceDiceBaby/Assets/Scenes/James/Scripts/SpellCostPanel.cs
+++ DiceDiceBaby/Assets/Scenes/James/Scripts/SpellCostPanel.cs
@@ -5,7 +5,18 @@ using UnityEngine.UI;
 
 public class SpellCostPanel : MonoBehaviour
 {
+    public Chris_Player player;
     public List<SpellManaPanel> spellMPanels;
 
-
+    public void UpdateSpellPanels()
+    {
+        foreach(SpellManaPanel smp in spellMPanels)
+        {
+            smp.disable();
+        }
+        for(int i = 0; i < player.ChosenSpell.costs.Count; i++)
+        {
+            spellMPanels[i].updateCost(player.ChosenSpell.costs[i]);
+        }
+    }
 }
